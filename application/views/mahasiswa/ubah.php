66ac79f13c88062a9c3267631f7b2a23d710b15a
adifens <dickyareda@gmail.com>
2020-03-11 02:35:01 +0700

    tambah ubah data dan search data

--- application/views/mahasiswa/ubah.php
+++ application/views/mahasiswa/ubah.php
@@ -3,37 +3,39 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    From Tambah Data Mahasiswa
+                    From Ubah Data Mahasiswa
                 </div>
                 <div class="card-body">
                     <form action="" method="POST">
+                        <input type="hidden" name="id" value="<?= $mahasiswa['id']; ?>">
                         <div class="form-group">
                             <label for="nama">Nama</label>
-                            <input type="text" class="form-control" id="nama" name="nama">
+                            <input type="text" class="form-control" id="nama" name="nama" value="<?= $mahasiswa['nama']; ?>">
                             <small class="form-text text-danger"><?= form_error('nama'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="NIM">NIM</label>
-                            <input type="text" class="form-control" id="nim" name="nim">
-                            <small class="form-text text-danger"><?= form_error('nim'); ?></small>
+                            <input type="text" class="form-control" id="nim" name="nim" value="<?= $mahasiswa['nim']; ?>">
+                            <small class=" form-text text-danger"><?= form_error('nim'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="text" class="form-control" id="email" name="email">
+                            <input type="text" class="form-control" id="email" name="email" value="<?= $mahasiswa['email']; ?>">
                             <small class="form-text text-danger"><?= form_error('email'); ?></small>
                         </div>
                         <div class="form-group">
                             <label for="jurusan">Jurusan</label>
                             <select class="form-control" id="jurusan" name="jurusan">
-                                <option value="Teknik Informatika">Teknik Informatika</option>
-                                <option value="Kedokteran">Kedokteran</option>
-                                <option value="Manajemen">Manajemen</option>
-                                <option value="PGSD">PGSD</option>
-                                <option value="Sastra Jepang">Sastra Jepang</option>
-
+                                <?php foreach ($jurusan as $j) : ?>
+                                    <?php if ($j == $mahasiswa['jurusan']) : ?>
+                                        <option value="<?= $j; ?>" selected><?= $j; ?></option>
+                                    <?php else : ?>
+                                        <option value="<?= $j; ?>"><?= $j; ?></option>
+                                    <?php endif; ?>
+                                <?php endforeach; ?>
                             </select>
                         </div>
-                        <button type="submit" name="tambah" class="btn btn-primary float-right">Tambah Data</button>
+                        <button type="submit" name="ubah" class="btn btn-primary float-right">Ubah Data</button>
                     </form>
                 </div>
             </div>
